72b9dfbb9ccfc224c99ab9a7489bd8654a3ec5e2
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-04-27 12:28:17 +0530

    write a code for  CreateUser routes

---
+++ routes/authRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const { createUser } = require("../controller/userCtrl");
+const router = express.Router();
+createUser;
+
+router.post("/register", createUser);
+
+module.exports = router;
