880eac82bf80fa11e144d6af6b1aab29958b2a8b
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-04-27 12:29:27 +0530

    require routes file

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+// require database connection
 require("./config/dbConnection").connect();
+// require route file
+const authRouter = require("./routes/authRoutes");
+
 const app = express();
 const dotenv = require("dotenv").config();
 const PORT = process.env.PORT || 8000;
 
-app.use("/", (req, resp) => {
-  resp.send("Hello from server side");
-});
+// middleware for body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// routes
+// authRoutes
+app.use("/api/user", authRouter);
 
 app.listen(PORT, (err) => {
   if (err) {
