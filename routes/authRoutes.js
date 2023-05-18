bdacd848ca335570451d805dd82984261e4f3a54
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-05-18 17:04:07 +0530

    routes for login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { createUser } = require("../controller/userCtrl");
+const { createUser, loginUserCtrl } = require("../controller/userCtrl");
 const router = express.Router();
 createUser;
 
+// route for create  a new user
 router.post("/register", createUser);
+
+// login route
+router.post("/login", loginUserCtrl);
 
 module.exports = router;
