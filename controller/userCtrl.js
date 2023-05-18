98ab32101362342a4dfdd83e9e4a8dc1c9748238
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-05-18 17:03:14 +0530

    code for login via email and password code

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -1,4 +1,6 @@
+const { generateToken } = require("../config/jwtToken");
 const User = require("../models/userModel");
+const asyncHandler = require("express-async-handler");
 // find user if already register if not register then  create a newUser
 const createUser = async (req, resp) => {
   const email = req.body.email;
@@ -8,11 +10,30 @@ const createUser = async (req, resp) => {
     const newUser = await User.create(req.body);
     resp.json(newUser);
   } else {
-    resp.json({
-      message: "user Already exists",
-      success: false,
-    });
+    throw new Error("user already Exists");
   }
 };
+// login using email and password
+const loginUserCtrl = asyncHandler(async (req, resp) => {
+  const { email, password } = req.body;
+  // console.log(email, password);
+  // check if user is already exists or not
+  const findUser = await User.findOne({ email });
+  if (findUser && (await findUser.isPasswordMatched(password))) {
+    // resp.json(findUser);
+    resp.json({
+      _id: findUser?._id,
+      firstname: findUser?.firstname,
+      lastname: findUser?.lastname,
+      email: findUser?.email,
+      mobile: findUser?.mobile,
+      token: generateToken(findUser._id),
+    });
+  } else {
+    resp.json({
+      message: "Invalid user",
+    });
+  }
+});
 
-module.exports = { createUser };
+module.exports = { createUser, loginUserCtrl };
