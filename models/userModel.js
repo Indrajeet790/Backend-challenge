32b425e452502a1e6b6c021e8692f3e468bea237
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-05-20 12:24:26 +0530

    update code

--- models/userModel.js
+++ models/userModel.js
@@ -6,12 +6,10 @@ var userSchema = new mongoose.Schema({
   firstname: {
     type: String,
     required: true,
-    unique: true,
   },
   lastname: {
     type: String,
     required: true,
-    unique: true,
   },
   email: {
     type: String,
