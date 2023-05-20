afc2917887ca4f9f199e2b401fc17661762403e2
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-05-20 12:27:34 +0530

    create a routes for /get a single user /routes for  gell all user/routes for delete a user/ routes for update a user

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,12 @@
 const express = require("express");
-const { createUser, loginUserCtrl } = require("../controller/userCtrl");
+const {
+  createUser,
+  loginUserCtrl,
+  getallUser,
+  getaUser,
+  deleteUser,
+  updateUser,
+} = require("../controller/userCtrl");
 const router = express.Router();
 createUser;
 
@@ -8,5 +15,17 @@ router.post("/register", createUser);
 
 // login route
 router.post("/login", loginUserCtrl);
+
+// route for gelAllUsers
+router.get("/all-users", getallUser);
+
+// get a single user
+router.get("/:id", getaUser);
+
+// delete route fo a user
+router.delete("/:id", deleteUser);
+
+// update a user details
+router.put("/:id", updateUser);
 
 module.exports = router;
