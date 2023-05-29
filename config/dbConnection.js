e17f7fcda564e1a08d88f60744d4d046eac43335
Indrajeet kumar gupta <indrajeetkumarnayak3@gmail.com>
2023-05-29 14:02:23 +0530

    updated code

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-require("dotenv").config();
+const dotenv = require("dotenv").config();
 mongoose.set("strictQuery", false);
 exports.connect = () => {
   mongoose
